d6a0162b75c6654e478f6ad75c67a6b4d5e5d65b
Christianah Adigun <chrissy-adigun@hotmail.com>
2020-07-06 00:43:31 -0500

    allow easy demo changes

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
-#======================================================================#
+Submitted to CS 8821 by Christianah Adigun, Sagari Raju Vatchhavayi & Nisarg Thakur.
 
-Christianah Adigun, Sagari Raju Vatchhavayi, Nisarg Thakur
-Steganography: Hiding and subsequently Retrieving Audio Files in Image
-#======================================================================#
+### Steganography: Hiding and subsequently Retrieving Audio Files in Image
+Our algorithm is inspired by the DrapsTv tutorial on hiding text in an image using the Least Significant bit of each pixel. Ours is a little more complex.
 
-Our algorithm is inspired by the DrapsTv tutorial on hiding text in an image using the Least Significant bit of each pixel. Ours is a little more complicated.
+## Getting Started
+Please install python2, pip & pillow to avoid errors. Does not work with python3.
 
-How it Works
--------------------
-For each pixel in the image, it gets the RGB sum and gets its modulo division of 3. It uses this information to determine whether it needs to modify the green value.
-The only limitation in our algorithm is that the embedded audio file size cannot exceed half the size of the image to be used. 
+## How it Works
+Our idea is that the RGB value of a pixel translates to yes or no to a simple question. Is the pixel RGB value divisible by 3? If the answer is yes, the bit of information that pixel represents is 1, and if no, it's 0. Until the expected padding/delimiter is found in the image, each pixel must provide a binary value. When this "encoded" binary is converted to hex, it's an audio file!
 
-For Linux Users:
-To encode, use the commands
+Some limitations: For best results, use an image file size that's at least 6x larger than audio file size. This tool works with any input audio file. However, the extracted file extension must be the same as input file extension. If your input file is '.mp3', ensure that you rename 'extracted' to 'extracted.mp3'. Happy information hiding!
+
+## encoding
 python ./main.py -e IMAGE_FILE
 
-
-To decode, use the commands
+## decoding
 python ./main.py -d IMAGE_FILE
+
+## Example
+```
+python2 ./main.py -e sample-image.jpg
+Enter path of audio file to hide: audio.mp3
+```
+
+```
+python2 ./main.py -d newfile.png
+```
+
+Thanks to pixabay.com for the royalty free image!
--- main.py
+++ main.py
@@ -1,115 +1,118 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 from PIL import Image
 import binascii, optparse
 
+delimiter = '110101010101010101010101010101011'
+ksize = len(delimiter)
+
 def getAudio2Str(filename):
-    audioArray = []
     message = ''
     audio = open(filename, 'rb')
-    with audio as f:
-        for line in f.readlines():
-            audioArray.append(str(line))
-    for i in range(0, len(audioArray)):
-        message += audioArray[i]
+    print('reading audio file...')
+    for line in audio.readlines():
+        message += str(line)
+    audio.close()
     return message
 
 
 def getStr2Audio(message):
-    with open('extracted.wav', 'wb') as file:
+    with open('extracted', 'wb') as file:
         file.write(message)
+    file.close()
+
 
 def str2bin(message):
+    binary = bin(int(binascii.hexlify(message), 16))
+    return binary[2:]+delimiter
 
-    binary = ''.join(format(ord(x), 'b').zfill(8) for x in message)
-    return binary+'111111001111111'
 
 def bin2str(binary):
-    message = ''
-    for i in range(0, len(binary), 8):
-        message += chr(int(binary[i:i+8],2))
+    message = binascii.unhexlify('%x' % (int('0b' + binary, 2)))
     return message
 
 def hide(filename, filepath):
     data = Image.open(filename).convert('RGBA')
     strAudio = getAudio2Str(filepath)
     binary = str2bin(strAudio)
-    print len(binary)
     newData = []
-    i = 0
+
     if data.mode in ('RGBA'):
         datas = data.getdata()
 
-    for item in datas:
-	pixel = item
+    i = 0
+    print('writing binary to image...')
+    for pixel in datas:
         if (i < len(binary)):
-		red   = pixel[0]
-            	green = pixel[1]
-            	blue  = pixel[2]
-            	sumof = red + green + blue
-            	if(sumof % 3 == 0):
-                	isOne = 1
-            	else:
-                	isOne = 0
-            	bit = binary[i]
-            	if (bit == '0' and isOne == 1):
-                	if(green > 250):
-				green = green - 1
-			else:
-				green = green + 1
-            	elif (bit == '1' and isOne == 0):
-                	if(green > 250):
-				green = green - (sumof%3)
-			else:
-				green = green + (3 - sumof%3)
-            	else:
-                	pass
-            	i += 1
-		pixel = (red, green, blue, 255)
-        	newData.append(pixel)
-	else:
-		newData.append(pixel)
-    data.putdata(newData)        	
+            red = pixel[0]
+            green = pixel[1]
+            blue = pixel[2]
+            sumof = red + green + blue
+            binsec = 0
+            if(sumof % 3 == 0):
+                binsec = 1
+            bit = binary[i]
+            if (bit == '0' and binsec == 1):
+                if(green > 250):
+                    green = green - 1
+                else:
+                    green = green + 1
+            elif (bit == '1' and binsec == 0):
+                if(green > 250):
+                    green = green - (sumof % 3)
+                else:
+                    green = green + (3 - sumof % 3)
+            i += 1
+            pixel = (red, green, blue, 255)
+            newData.append(pixel)
+        else:
+            newData.append(pixel)
+
+    data.putdata(newData)
     data.save("newfile.png", "PNG")
     return "Completed!"
 
+
 def retr(filename):
-	data = Image.open(filename).convert('RGBA')
-	binary = ''
-	if data.mode in ('RGBA'):
-        	datas = data.getdata()
-    	for item in datas:
-        	pixel = item
-        	red   = pixel[0]
-        	green = pixel[1]
-        	blue  = pixel[2]
-        	sumof = red + green + blue
-        	if (sumof % 3 == 0):
-                	bit = 1
-        	else:
-                	bit = 0
-        	binary += str(bit)
-        	if (binary[-17:] == '11111110011111111'):
-                	binary = binary[:-17]
-			print len(binary)
-                	break
-	return getStr2Audio(bin2str(binary))
+    data = Image.open(filename).convert('RGBA')
+    binary = ''
+    if data.mode in ('RGBA'):
+        datas = data.getdata()
+    print('extracting audio from image...')
+    for item in datas:
+        pixel = item
+        red = pixel[0]
+        green = pixel[1]
+        blue = pixel[2]
+        sumof = red + green + blue
+        if (sumof % 3 == 0):
+            bit = 1
+        else:
+            bit = 0
+        binary += str(bit)
+        if (binary[-ksize:] == delimiter):
+            binary = binary[:-ksize]
+            break
+    getStr2Audio(bin2str(binary))
+    return 'done'
+
 
 def Main():
-    parser = optparse.OptionParser('usage %prog '+\
-    	'-e/-d <target file>')
-    parser.add_option('-e', dest='hide', type='string', \
-    	help='target picture path to hide audio')
-    parser.add_option('-d', dest='retr', type='string', \
+    parser = optparse.OptionParser('usage %prog ' +
+                                   '-e/-d <target file>')
+    parser.add_option('-e', dest='hide', type='string',
+                      help='target picture path to hide audio')
+    parser.add_option('-d', dest='retr', type='string',
                       help='target picture path to retrieve audio')
     (options, args) = parser.parse_args()
     if (options.hide != None):
         filepath = raw_input("Enter path of audio file to hide: ")
-        print hide(options.hide, filepath)
+        print(hide(options.hide, filepath))
     elif (options.retr != None):
-        print retr(options.retr)
+        print(retr(options.retr))
     else:
-        print parser.usage
+        print(parser.usage)
         exit(0)
+
 
 if __name__ == '__main__':
     Main()
